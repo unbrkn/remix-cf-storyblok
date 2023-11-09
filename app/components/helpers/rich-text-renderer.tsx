a75c98fc9face5665cf49f933cebc7e1846f97df
Emanuel Imhof <emanuel@sahli.io>
2023-11-09 14:08:18 +0100

    feature: add rich-text-renderer component

---
+++ app/components/helpers/rich-text-renderer.tsx
@@ -0,0 +1,20 @@
+import {render} from 'storyblok-rich-text-react-renderer-ts'
+import {StoryblokComponent} from '@storyblok/react/rsc'
+
+type RichTextRendererProps = {
+    text: any
+}
+
+const RichTextRenderer = ({text}: RichTextRendererProps) => {
+    return (
+        <>
+            {render(text, {
+                defaultBlokResolver: (name, props) => (
+                    <StoryblokComponent blok={{component: name, ...props}}/>
+                ),
+            })}
+        </>
+    )
+}
+
+export default RichTextRenderer
